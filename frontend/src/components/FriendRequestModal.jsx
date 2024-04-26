23a193b16b1ab47d5ee6e781a23e25485a5352fe
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2024-04-26 21:00:12 +0530

    added accept friend in finding user. made some changes in sidebar and message panel header

--- frontend/src/components/FriendRequestModal.jsx
+++ frontend/src/components/FriendRequestModal.jsx
@@ -1,32 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { FaCheck } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
 
-import { socket } from '../socket'
+import { socket } from "../socket";
 
-
-import { USER_ID } from '../requestMethod'
+import { USER_ID } from "../requestMethod";
 
 const FriendRequestModal = ({ isOpen, closeModal }) => {
+  const [recipient, setRecipient] = useState();
 
-    const [recipient, setRecipient] = useState()
-
-    const handleSend = async (e) => {
-        try {
-            // const res = await userRequest.post("/user/send-friend-request", { recipient: recipient })
-            // console.log(res)
-            socket.emit("friend-request", { sender: USER_ID, recipient: recipient })
-        } catch (error) {
-            console.error(error)
-        }
+  const handleSend = async (e) => {
+    try {
+      // const res = await userRequest.post("/user/send-friend-request", { recipient: recipient })
+      // console.log(res)
+      socket.emit("friend-request", { sender: USER_ID, recipient: recipient });
+      closeModal();
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-
-
-
-
-    return (
-        <>
-            {isOpen && (<div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                {/* <!--
+  return (
+    <>
+      {isOpen && (
+        <div
+          className="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          {/* <!--
             Background backdrop, show/hide based on modal state.
 
             Entering: "ease-out duration-300"
@@ -36,11 +39,11 @@ const FriendRequestModal = ({ isOpen, closeModal }) => {
             From: "opacity-100"
             To: "opacity-0"
   --> */}
-                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        {/* <!--
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              {/* <!--
                     Modal panel, show/hide based on modal state.
 
                     Entering: "ease-out duration-300"
@@ -50,23 +53,207 @@ const FriendRequestModal = ({ isOpen, closeModal }) => {
                     From: "opacity-100 translate-y-0 sm:scale-100"
                     To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       --> */}
-                        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                <form onSubmit={null}>
-                                    <input type="text" name='recipient' onChange={(e) => setRecipient(e.target.value)} className='px-4 w-full py-2 border rounded border-black' placeholder='Enter Username or Email..' />
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <form onSubmit={null}>
+                    <input
+                      type="text"
+                      name="recipient"
+                      onChange={(e) => setRecipient(e.target.value)}
+                      className="px-4 w-full py-2 border rounded border-black"
+                      placeholder="Enter Username or Email.."
+                    />
+                  </form>
 
-                                </form>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                <button onClick={handleSend} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Send</button>
-                                <button type="button" onClick={closeModal} className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-                            </div>
-                        </div>
+                  <div
+                    style={{ scrollbarWidth: "none" }}
+                    className="mt-3 max-h-[9rem] overflow-y-scroll "
+                  >
+                    <div className="flex gap-4 items-center w-full mb-2">
+                      <figure className=" h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex flex-col justify-center">
+                        <span className="text-gray-500 text-sm">
+                          Priyanshu sahu
+                        </span>
+                        <span className="font-semibold  text-xs">
+                          +91-8839128532
+                        </span>
+                      </div>
+                      <div className="flex-grow" />
+                      <div className="flex w-fit items-center text-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-8 h-8 mr-1 rounded-full py-2 px-2 justify-center bg-[#3a4dfc] text-white shadow-sm hover:bg-gray-50 hover:text-zinc-700 hover:ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                        >
+                          <FaCheck />
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-8 h-8 rounded-full py-2 px-2 justify-center  bg-white  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-100  hover:ring-0 sm:mt-0 sm:w-auto"
+                        >
+                          <RxCross2 className="" />
+                        </button>
+                      </div>
                     </div>
+                    <div className="flex gap-4 items-center w-full mb-2">
+                      <figure className=" h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex flex-col justify-center">
+                        <span className="text-gray-500 text-sm">
+                          Priyanshu sahu
+                        </span>
+                        <span className="font-semibold  text-xs">
+                          +91-8839128532
+                        </span>
+                      </div>
+                      <div className="flex-grow" />
+                      <div className="flex w-fit items-center text-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-8 h-8 mr-1 rounded-full py-2 px-2 justify-center bg-[#3a4dfc] text-white shadow-sm hover:bg-gray-50 hover:text-zinc-700 hover:ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                        >
+                          <FaCheck />
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-8 h-8 rounded-full py-2 px-2 justify-center  bg-white  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-100  hover:ring-0 sm:mt-0 sm:w-auto"
+                        >
+                          <RxCross2 className="" />
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex gap-4 items-center w-full mb-2">
+                      <figure className=" h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex flex-col justify-center">
+                        <span className="text-gray-500 text-sm">
+                          Priyanshu sahu
+                        </span>
+                        <span className="font-semibold  text-xs">
+                          +91-8839128532
+                        </span>
+                      </div>
+                      <div className="flex-grow" />
+                      <div className="flex w-fit items-center text-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-8 h-8 mr-1 rounded-full py-2 px-2 justify-center bg-[#3a4dfc] text-white shadow-sm hover:bg-gray-50 hover:text-zinc-700 hover:ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                        >
+                          <FaCheck />
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-8 h-8 rounded-full py-2 px-2 justify-center  bg-white  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-100  hover:ring-0 sm:mt-0 sm:w-auto"
+                        >
+                          <RxCross2 className="" />
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex gap-4 items-center w-full mb-2">
+                      <figure className=" h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex flex-col justify-center">
+                        <span className="text-gray-500 text-sm">
+                          Priyanshu sahu
+                        </span>
+                        <span className="font-semibold  text-xs">
+                          +91-8839128532
+                        </span>
+                      </div>
+                      <div className="flex-grow" />
+                      <div className="flex w-fit items-center text-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-8 h-8 mr-1 rounded-full py-2 px-2 justify-center bg-[#3a4dfc] text-white shadow-sm hover:bg-gray-50 hover:text-zinc-700 hover:ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                        >
+                          <FaCheck />
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-8 h-8 rounded-full py-2 px-2 justify-center  bg-white  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-100  hover:ring-0 sm:mt-0 sm:w-auto"
+                        >
+                          <RxCross2 className="" />
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex gap-4 items-center w-full mb-2">
+                      <figure className=" h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover"
+                          src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                          alt=""
+                        />
+                      </figure>
+                      <div className="flex flex-col justify-center">
+                        <span className="text-gray-500 text-sm">
+                          Priyanshu sahu
+                        </span>
+                        <span className="font-semibold  text-xs">
+                          +91-8839128532
+                        </span>
+                      </div>
+                      <div className="flex-grow" />
+                      <div className="flex w-fit items-center text-center">
+                        <button
+                          type="button"
+                          className="inline-flex w-8 h-8 mr-1 rounded-full py-2 px-2 justify-center bg-[#3a4dfc] text-white shadow-sm hover:bg-gray-50 hover:text-zinc-700 hover:ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                        >
+                          <FaCheck />
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex w-8 h-8 rounded-full py-2 px-2 justify-center  bg-white  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-100  hover:ring-0 sm:mt-0 sm:w-auto"
+                        >
+                          <RxCross2 className="" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </div>)}
-        </>
-    )
-}
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    onClick={handleSend}
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-[#3a4dfc] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#4447ff] sm:ml-3 sm:w-auto"
+                  >
+                    Send
+                  </button>
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default FriendRequestModal
+export default FriendRequestModal;
--- frontend/src/components/SideChatbar.jsx
+++ frontend/src/components/SideChatbar.jsx
@@ -1,61 +1,86 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 // import { BsFillPlusCircleFill } from "react-icons/bs";
 import { IoAdd } from "react-icons/io5";
-import FriendRequestModal from './FriendRequestModal';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentChat } from '../redux/appRedux';
+import FriendRequestModal from "./FriendRequestModal";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentChat } from "../redux/appRedux";
 const SideChatbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    // const [conversations, setConversations] = useState([])
+  const [isOpen, setIsOpen] = useState(false);
+  // const [conversations, setConversations] = useState([])
 
-    const conversation = useSelector(state => state.user.conversations)
+  const conversation = useSelector((state) => state.user.conversations);
 
- 
-    const openModal = () => {
-        setIsOpen(true);
-    };
+  const openModal = () => {
+    setIsOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsOpen(false);
-    };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
+  const handleCurrChat = (id) => {
+    dispatch(setCurrentChat(id));
+  };
 
-
-    const handleCurrChat = (id) => {
-        dispatch(setCurrentChat(id))
-    }
-
-    
-    return (
-        <div className='w-96 bg-white h-screen border-r'>
-            <FriendRequestModal openModal={openModal} closeModal={closeModal} isOpen={isOpen} />
-            <section className='flex justify-between items-center px-6 py-4 border-b'>
-                <h1 className='font-[poppins] font-semibold text-lg -tracking-tight flex items-center'>Messages <IoIosArrowDown className='mt-1' /></h1>
-                <div className='p-1 bg-[#615ef0] rounded-full h-fit cursor-pointer' onClick={openModal}>
-                    <IoAdd size={20} color='white' />
-                </div>
-            </section>
-            <section className='flex flex-col px-4 '>
-
-                {conversation?.map((data) => {
-                    return (<section onClick={() => { handleCurrChat(data) }} key={data._id} className='flex items-center gap-4 py-5 px-4  hover:bg-[#ededf9] cursor-pointer rounded-lg'>
-                        <figure className=' h-12 rounded-full'>
-                            <img className='rounded-full w-full h-full object-cover' src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg" alt="Profile" />
-                        </figure>
-                        <section className='flex flex-col w-full'>
-                            <span className='flex justify-between items-center w-full'><span className='text-base font-medium text-black'>Priyanshu Sahu</span> <span className='text-xs text-gray-400'>12m</span></span>
-                            {/* <span className='text-xs text-gray-600'>+91 8839 128 532</span> */}
-                            <span className='text-xs text-gray-400'>You: <span className='text-gray-600 font-medium'>I Am HEHHEHHE</span></span>
-                        </section>
-                    </section>)
-                })}
-            </section>
-
+  return (
+    <div className="w-96 bg-white h-screen border-r">
+      <FriendRequestModal
+        openModal={openModal}
+        closeModal={closeModal}
+        isOpen={isOpen}
+      />
+      <section className="flex justify-between items-center px-6 py-5 border-b">
+        <h1 className="font-[poppins] font-semibold -tracking-tight flex items-center">
+          Messages <IoIosArrowDown className="mt-1" />
+        </h1>
+        <div
+          className="p-1 bg-[#3a4dfc] rounded-full h-fit cursor-pointer"
+          onClick={openModal}
+        >
+          <IoAdd size={20} color="white" />
         </div>
-    )
-}
+      </section>
+      <section className="flex flex-col px-4 ">
+        {conversation?.map((data) => {
+          return (
+            <section
+              onClick={() => {
+                handleCurrChat(data);
+              }}
+              key={data._id}
+              className="flex items-center gap-4 py-5 px-4  hover:bg-[#ededf9] cursor-pointer rounded-lg"
+            >
+              <figure className=" h-12 rounded-full">
+                <img
+                  className="rounded-full w-full h-full object-cover"
+                  src="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+                  alt="Profile"
+                />
+              </figure>
+              <section className="flex flex-col w-full">
+                <span className="flex justify-between items-center w-full">
+                  <span className="text-base font-medium text-black">
+                    Priyanshu Sahu
+                  </span>{" "}
+                  <span className="text-xs text-gray-400">12m</span>
+                </span>
+                {/* <span className='text-xs text-gray-600'>+91 8839 128 532</span> */}
+                <span className="text-xs text-gray-400">
+                  You:{" "}
+                  <span className="text-gray-600 font-medium">
+                    I Am HEHHEHHE
+                  </span>
+                </span>
+              </section>
+            </section>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
 
-export default SideChatbar  
+export default SideChatbar;
